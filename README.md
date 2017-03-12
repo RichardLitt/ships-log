ce2bf5c10d0b8e12b08df41d8f91d51a8ba27777
Richard Littauer <richard.littauer@gmail.com>
2017-03-12 13:44:11 -0400

    Init

---
+++ README.md
@@ -0,0 +1,28 @@
+# ships-log
+
+> Open up a log file for today in a project folder
+
+## Install
+
+```sh
+npm i -g project-logg
+```
+
+## Usage
+
+```sh
+log
+# Will automatically open up today's date file in log/
+
+# Or, to see yesterday's tasks
+log --yesterday
+# Will print them to stdout
+```
+
+## Contribute
+
+This is mainly for me. Please open an issue if you want to contribute, first.
+
+## License
+
+MIT © 2017 Richard Littauer
---
+++ index.js
@@ -0,0 +1,80 @@
+#!bin/usr/env node
+
+const fs = require('fs')
+const path = require('path')
+const moment = require('moment')
+const pify = require('pify')
+const mkdirp = pify(require('mkdirp'))
+const writeFile = require('write')
+const opn = require('opn')
+const fileExists = require('file-exists')
+const meow = require('meow')
+
+const today = moment().format('YYYY-MM-DD')
+const logDir = path.resolve(__dirname, 'log')
+const todayFile = `${logDir}/${today}.md`
+const divider = `\n## Next\n`
+
+function getYesterday () {
+  return pify(fs.readdir)(logDir).then(res => {
+    if (res.length === 0) {
+      throw new Error('No files in log directory')
+    }
+    return res[res.length - 1]
+  }).then(res => {
+    return pify(fs.readFile)(path.resolve(__dirname, `log/${res}`), 'utf8').then(res => {
+      return res.split(divider)[1]
+    }).catch(err => {
+      console.log('Unable to get tasks from yesterday.')
+      return err
+    })
+  }).catch(err => {
+    return err
+  })
+}
+
+function createToday () {
+  return mkdirp(logDir).then((res) => {
+    return pify(fileExists)(todayFile)
+  }).catch(err => {
+    if (err.code === 'ENOENT') {
+      return getYesterday().then(res => {
+        return pify(writeFile)(todayFile, `# ${today}
+
+## Mission
+
+## To Do
+${(typeof res === 'string') ? res : ''}
+## Roundup
+
+## Next
+`)
+      })
+    }
+  }).then(res => {
+    console.log('Opening file...')
+    opn(todayFile, {app: process.env.IDE})
+    process.exit(0)
+  })
+}
+
+const cli = meow([`
+  Usage
+    $ project
+
+  Examples
+    $ options
+    Opening file...
+`], {})
+
+if (cli.flags.yesterday) {
+  getYesterday().then(res => {
+    if (res.length === 0) {
+      console.log("I don't remember yesterday.")
+    } else {
+      console.log(res)
+    }
+  })
+} else {
+  createToday()
+}
